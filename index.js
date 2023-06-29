41f4e164b1445d4485c8006feced11b653fed280
Rajnish Tripathi <rajnishtripathi2001@gmail.com>
2023-06-29 21:28:32 +0530

    subtract api

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const app = express();
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
+app.get("/sum", (req, res) => {
   if (req.query.a == null || req.query.b == null) {
     res.status(400).send({ error: "Number is null" });
   }
   var a = Number(req.query.a);
   var b = Number(req.query.b);
-  var name = req.query.name;
 
-  console.log(name);
   const sum = a + b;
-  res.status(200).send({ Sum: sum, Name: name });
+  res.status(200).send({ Sum: sum});
 });
+
+app.get("/sub", (req, res) => {
+    if (req.query.a == null || req.query.b == null) {
+      res.status(400).send({ error: "Number is null" });
+    }
+    var a = Number(req.query.a);
+    var b = Number(req.query.b);
+  
+    const sub= a - b;
+    res.status(200).send({ Sub: sub });
+  });
 
 app.listen(5000, () => {
   console.log("Server Running at port : 5000");
