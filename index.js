68bbe2ba8c0c045167ff0af4de08dca947062451
Rajnish Tripathi <82767514+rajnishtripathi2001@users.noreply.github.com>
2023-06-29 22:39:53 +0530

    Multiply (#3)
    * subtract api
    
    * multiply added

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ app.get("/sum", (req, res) => {
   var b = Number(req.query.b);
 
   const sum = a + b;
+
+  res.status(200).send({ Sum: sum });
+});
+
+
+
+app.get("/mul", (req, res) => {
+  if (req.query.a == null || req.query.b == null) {
+    res.status(400).send({ error: "Number is null" });
+  }
+  var a = Number(req.query.a);
+  var b = Number(req.query.b);
+
+  const mul = a * b;
+  res.status(200).send({ Multi: mul });
+
 });
 
 app.get("/sub", (req, res) => {
