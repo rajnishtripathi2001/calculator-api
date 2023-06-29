fb2047551e7bcb79a0f404db32e8f0f35e689040
Rajnish Tripathi <rajnishtripathi2001@gmail.com>
2023-06-29 22:11:33 +0530

    multiply added

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.use(express.json());
 
-
 app.get("/sum", (req, res) => {
   if (req.query.a == null || req.query.b == null) {
     res.status(400).send({ error: "Number is null" });
@@ -12,21 +11,31 @@ app.get("/sum", (req, res) => {
   var b = Number(req.query.b);
 
   const sum = a + b;
-  res.status(200).send({ Sum: sum});
+  res.status(200).send({ Sum: sum });
 });
 
 app.get("/sub", (req, res) => {
-    if (req.query.a == null || req.query.b == null) {
-      res.status(400).send({ error: "Number is null" });
-    }
-    var a = Number(req.query.a);
-    var b = Number(req.query.b);
-  
-    const sub= a - b;
-    res.status(200).send({ Sub: sub });
-  });
+  if (req.query.a == null || req.query.b == null) {
+    res.status(400).send({ error: "Number is null" });
+  }
+  var a = Number(req.query.a);
+  var b = Number(req.query.b);
+
+  const sub = a - b;
+  res.status(200).send({ Sub: sub });
+});
+
+app.get("/mul", (req, res) => {
+  if (req.query.a == null || req.query.b == null) {
+    res.status(400).send({ error: "Number is null" });
+  }
+  var a = Number(req.query.a);
+  var b = Number(req.query.b);
+
+  const mul = a * b;
+  res.status(200).send({ Multi: mul });
+});
 
 app.listen(5000, () => {
   console.log("Server Running at port : 5000");
 });
-
