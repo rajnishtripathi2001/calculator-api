e924a2d5c9e65167a732d51c48932a50a8be6b18
Rajnish Tripathi <82767514+rajnishtripathi2001@users.noreply.github.com>
2023-06-29 22:35:22 +0530

    subtract api (#1)

--- index.js
+++ index.js
@@ -11,30 +11,18 @@ app.get("/sum", (req, res) => {
   var b = Number(req.query.b);
 
   const sum = a + b;
-  res.status(200).send({ Sum: sum });
 });
 
 app.get("/sub", (req, res) => {
-  if (req.query.a == null || req.query.b == null) {
-    res.status(400).send({ error: "Number is null" });
-  }
-  var a = Number(req.query.a);
-  var b = Number(req.query.b);
-
-  const sub = a - b;
-  res.status(200).send({ Sub: sub });
-});
-
-app.get("/mul", (req, res) => {
-  if (req.query.a == null || req.query.b == null) {
-    res.status(400).send({ error: "Number is null" });
-  }
-  var a = Number(req.query.a);
-  var b = Number(req.query.b);
-
-  const mul = a * b;
-  res.status(200).send({ Multi: mul });
-});
+    if (req.query.a == null || req.query.b == null) {
+      res.status(400).send({ error: "Number is null" });
+    }
+    var a = Number(req.query.a);
+    var b = Number(req.query.b);
+  
+    const sub= a - b;
+    res.status(200).send({ Sub: sub });
+  });
 
 app.listen(5000, () => {
   console.log("Server Running at port : 5000");
